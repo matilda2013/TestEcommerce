2b096f91da3876c40dfc3035d46dd336fb28159b
hola.holu101@gmail.com <hola.holu101@gmail.com>
2022-12-08 11:25:29 +0100

    Initial Commit

---
+++ AngEcommerceApi/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using AngEcommerceApi.Data;
+using AngEcommerceApi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngEcommerceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly MyAppContext _context;
+        public ProductController(MyAppContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+
+    }
+}
---
+++ AngEcommerceApi/Data/MyAppContext.cs
@@ -0,0 +1,22 @@
+﻿using AngEcommerceApi.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngEcommerceApi.Data
+{
+    public class MyAppContext : DbContext
+    {
+
+        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
